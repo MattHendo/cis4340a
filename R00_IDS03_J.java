af8c6d99165530a23deb2bfe693c68e8764d2b4e
Matthew Nighting-Henderson <matt.henderson2004@knights.ucf.edu>
2025-02-28 13:40:03 -0500

    Fixed noncompliant example 1

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,11 +1,15 @@
-// Noncompliant Example
-public class Main {
-    if (loginSuccessful) {
-        logger.severe("User login succeeded for: " + username); // Logs sensitive info
-    } else {
-        logger.severe("User login failed for: " + username);
-    }
+//Compliant Example
+
+if (loginSuccessful) {
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
+} else {
+  logger.severe("User login failed for: " + sanitizeUser(username));
 }
 
+
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
+}
 
 
