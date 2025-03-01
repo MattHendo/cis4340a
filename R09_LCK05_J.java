0ce3d1fa28856303ec5af8eb0d10201c59e56a48
Matthew Nighting-Henderson <matt.henderson2004@knights.ucf.edu>
2025-03-01 17:21:14 -0500

    Added noncompliant example 8

---
+++ R09_LCK05_J.java
@@ -0,0 +1,9 @@
+//Noncompliant Example 
+/* This class is not thread-safe */
+public final class CountHits {
+    private static int counter;
+   
+    public void incrementCounter() {
+      counter++;
+    }
+  }
